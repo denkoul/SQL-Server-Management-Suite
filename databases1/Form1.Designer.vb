56cbf5c4f4c3c38dac8fb9badabe290823535408
Dionysios Koulouris <dennis.koulouris@outlook.com>
2019-03-21 13:24:55 +0200

    Add files via upload

---
+++ databases1/Form1.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Password = New System.Windows.Forms.TextBox()
+        Me.Connect = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.username = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ip = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 230)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(313, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 214)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(122, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter ""select"" command"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(344, 230)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Execute"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 290)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(407, 199)
+        Me.ListBox1.TabIndex = 8
+        '
+        'Password
+        '
+        Me.Password.Location = New System.Drawing.Point(12, 124)
+        Me.Password.Name = "Password"
+        Me.Password.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.Password.Size = New System.Drawing.Size(313, 20)
+        Me.Password.TabIndex = 3
+        '
+        'Connect
+        '
+        Me.Connect.Location = New System.Drawing.Point(344, 124)
+        Me.Connect.Name = "Connect"
+        Me.Connect.Size = New System.Drawing.Size(75, 23)
+        Me.Connect.TabIndex = 4
+        Me.Connect.Text = "Connect"
+        Me.Connect.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Enter password"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 66)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(81, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Enter username"
+        '
+        'username
+        '
+        Me.username.Location = New System.Drawing.Point(12, 85)
+        Me.username.Name = "username"
+        Me.username.Size = New System.Drawing.Size(313, 20)
+        Me.username.TabIndex = 2
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 21)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(106, 13)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Enter the ip of the db"
+        '
+        'ip
+        '
+        Me.ip.Location = New System.Drawing.Point(12, 40)
+        Me.ip.Name = "ip"
+        Me.ip.Size = New System.Drawing.Size(313, 20)
+        Me.ip.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 504)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 17
+        Me.Button1.Text = "Reseed"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 165)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(313, 21)
+        Me.ComboBox1.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 149)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(86, 13)
+        Me.Label3.TabIndex = 20
+        Me.Label3.Text = "Select Database"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 257)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(313, 23)
+        Me.Button3.TabIndex = 21
+        Me.Button3.Text = "List current's tables"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(93, 506)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(213, 20)
+        Me.TextBox2.TabIndex = 24
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.ClientSize = New System.Drawing.Size(434, 548)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ip)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.username)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Connect)
+        Me.Controls.Add(Me.Password)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Password As TextBox
+    Friend WithEvents Connect As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents username As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ip As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ databases1/Form1.vb
@@ -0,0 +1,266 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+
+    Dim command As New SqlCommand
+    Dim data_reader As SqlDataReader
+    Dim connection_string As String
+    Dim con As New SqlConnection()
+    Dim local As Boolean = False
+    Dim output As String
+
+    Dim local_filename As String
+    Dim c As Int32
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+
+    Sub reseed(ByVal ConnectionString As String, ByVal TableName As String)
+        Dim con1 As New System.Data.SqlClient.SqlConnection()
+        con1.ConnectionString = ConnectionString
+        con1.Open()
+        Dim parents As New List(Of String)()
+        Try
+            Dim delete_fk As New System.Data.SqlClient.SqlCommand("select object_name(parent_object_id)
+            from sys.foreign_keys
+            WHERE object_name(referenced_object_id) ='" & TableName & "'", con1)
+            Try
+                Dim dr = delete_fk.ExecuteReader()
+                While dr.Read()
+                    parents.Add(dr(0).ToString())
+                End While
+                dr.Close()
+            Catch ex As System.Data.SqlClient.SqlException
+                MessageBox.Show("There was an error accessing your data. DETAIL: " & ex.ToString())
+            End Try
+            For Each table As String In parents
+                Dim remove_fk As New System.Data.SqlClient.SqlCommand("ALTER TABLE " & table.ToString & " NOCHECK CONSTRAINT ALL", con1)
+                remove_fk.ExecuteNonQuery()
+            Next
+            Dim copy_back As String = "insert into " + TableName + " select * from #temp1"
+            Dim first_copy As New System.Data.SqlClient.SqlCommand("Select * into #temp1 from " + TableName, con1)
+            Dim delete_Table As New System.Data.SqlClient.SqlCommand("delete from " + TableName, con1)
+            Dim second_copy As New System.Data.SqlClient.SqlCommand(copy_back, con1)
+            first_copy.ExecuteNonQuery()
+            delete_Table.ExecuteNonQuery()
+            second_copy.ExecuteNonQuery()
+            For Each table As String In parents
+                Dim regain_fk As New SqlCommand("ALTER TABLE " & table.ToString & " WITH CHECK CHECK CONSTRAINT ALL", con1)
+                regain_fk.ExecuteNonQuery()
+            Next
+        Catch ex As SqlException
+            MessageBox.Show("Can Not execute command", "Error", MessageBoxButtons.OK)
+        End Try
+        con1.Close()
+    End Sub
+
+
+
+
+
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ListBox1.Items.Clear()
+        con.Open()
+        command.CommandText = TextBox1.Text
+        Try
+            data_reader = command.ExecuteReader()
+            If data_reader.HasRows Then
+                While (data_reader.Read())
+
+                    c = 0
+                    output = " "
+                    While c < data_reader.FieldCount
+
+                        output = output + " " + Convert.ToString(data_reader.GetSqlValue(c))
+                        c = c + 1
+                    End While
+
+
+                    ListBox1.Items.Add(output)
+                End While
+            End If
+        Catch ex As SqlException
+            MessageBox.Show("Invalid syntax", "Error", MessageBoxButtons.OK)
+        End Try
+
+        con.Close()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button2.Enabled = False
+
+    End Sub
+
+    Private Sub Connect_Click(sender As Object, e As EventArgs) Handles Connect.Click
+
+        con.ConnectionString = "Data Source=" + ip.Text + ";User ID=" + username.Text + ";Password=" + Password.Text
+
+
+
+
+
+
+
+
+        command.Connection = con
+
+
+
+        Try
+
+            con.Open()
+
+            MessageBox.Show("Connection successful", "Connected", MessageBoxButtons.OK)
+            Button2.Enabled = True
+            Dim select_data
+            command.CommandText = "Select name FROM master.dbo.sysdatabases"
+
+            data_reader = command.ExecuteReader()
+            While data_reader.Read
+                ComboBox1.Items.Add(data_reader("name").ToString())
+            End While
+
+
+
+            con.Close()
+        Catch ex As SqlException
+            MessageBox.Show("Wrong login details.", "Error", MessageBoxButtons.OK)
+        End Try
+
+
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim table_to_resed As String = TextBox2.Text
+        con.Open()
+        Dim parents As New List(Of String)()
+
+        Try
+
+            Dim delete_fk As New SqlCommand("select object_name(parent_object_id)
+            from sys.foreign_keys
+            WHERE object_name(referenced_object_id) ='" & table_to_resed & "'", con)
+
+            Try
+                Dim dr = delete_fk.ExecuteReader()
+                While dr.Read()
+                    parents.Add(dr(0).ToString())
+                End While
+                dr.Close()
+            Catch ex As SqlException
+                ' Do some logging or something. 
+                MessageBox.Show("There was an error accessing your data. DETAIL: " & ex.ToString())
+            End Try
+
+            For Each table As String In parents
+                Dim remove_fk As New SqlCommand("ALTER TABLE " & table.ToString & " NOCHECK CONSTRAINT ALL", con)
+                remove_fk.ExecuteNonQuery()
+            Next
+
+            Dim copy_back As String = "insert into " + table_to_resed + " select * from #temp1  "
+
+
+            Dim first_copy As New SqlCommand("Select * into #temp1 from " + table_to_resed, con)
+            Dim delete_Table As New SqlCommand("delete from " + table_to_resed, con)
+            Dim second_copy As New SqlCommand(copy_back, con)
+
+
+            first_copy.ExecuteNonQuery()
+            delete_Table.ExecuteNonQuery()
+            second_copy.ExecuteNonQuery()
+
+            For Each table As String In parents
+                Dim regain_fk As New SqlCommand("ALTER TABLE " & table.ToString & " WITH CHECK CHECK CONSTRAINT ALL", con)
+                regain_fk.ExecuteNonQuery()
+            Next
+
+        Catch ex As SqlException
+            MessageBox.Show("Can Not execute command", "Error", MessageBoxButtons.OK)
+        End Try
+
+        con.Close()
+
+
+
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+
+
+        Try
+
+            con.ConnectionString = "Data Source=" & ip.Text + ";Initial Catalog=" + ComboBox1.SelectedItem.ToString + ";User ID=" + username.Text + ";Password=" + Password.Text
+
+
+            command.Connection = con
+            Try
+                con.Open()
+
+                MessageBox.Show("Database changed To " & ComboBox1.SelectedItem.ToString, "Connected", MessageBoxButtons.OK)
+                Button2.Enabled = True
+                Button3.Text = "List " & ComboBox1.SelectedItem.ToString & "'s tables"
+
+
+
+
+
+
+
+                con.Close()
+            Catch ex As SqlException
+                MessageBox.Show("Wrong login details", "Error", MessageBoxButtons.OK)
+            End Try
+
+
+        Catch ex As Exception
+            MsgBox("An error occurced. Try again")
+
+        End Try
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Text = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'"
+        Me.Button2.PerformClick()
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs)
+        Dim dialog As New OpenFileDialog()
+        If DialogResult.OK = dialog.ShowDialog Then
+            local_filename = dialog.FileName
+        End If
+
+        ip.Enabled = False
+        local = True
+
+
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+End Class
